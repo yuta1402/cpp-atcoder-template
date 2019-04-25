f429da83dc113954f92b5c67047a66058dacf390
gut <yuta1402souja@live.jp>
2019-04-25 11:47:44 +0900

    Add template

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <limits>
+#include <climits>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+int main()
+{
+    return 0;
+}
